5375a3a77ed4c579852721166f931cf5a6f39473
StephenDay1 <57306301+StephenDay1@users.noreply.github.com>
2021-08-25 06:23:38 -0600

    TurnAngle now uses PID from AlignShooter

--- src/main/java/frc/robot/commands/Drivetrain_Commands/TurnAngle.java
+++ src/main/java/frc/robot/commands/Drivetrain_Commands/TurnAngle.java
@@ -4,64 +4,81 @@
 
 package frc.robot.commands.Drivetrain_Commands;
 
+import java.util.function.DoubleConsumer;
+import java.util.function.DoubleSupplier;
+
 import com.ctre.phoenix.motorcontrol.ControlMode;
 
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.PIDCommand;
 import frc.robot.subsystems.DrivetrainFalcon;
 
-public class TurnAngle extends CommandBase {
+public class TurnAngle extends PIDCommand {
   /** Creates a new TurnAngle. 
    * 
    * Angle is measured from the front of the robot, going counterclockwise
   */
   private DrivetrainFalcon drivetrain;
+  private final double kP = 1.0; // Value needs tuned
 
-  private int allowedError = 2; // Two degrees of error
-  private double degrees;
-  private double leftVelocity = .2;
-  private double rightVelocity = .2;
-  public TurnAngle(DrivetrainFalcon drivetrain, double degrees) {
-    // Use addRequirements() here to declare subsystem dependencies.
+  private double decay = -.5; // Value needs tuned
+  private double prev_value = 0;
+
+  private Timer timer = new Timer();
+
+  private AlignMotorFeedforward m_ff = new AlignMotorFeedforward(.18, 0.0); // Values need tuned
+
+  public TurnAngle(DrivetrainFalcon drivetrain, PIDController controller, DoubleSupplier measurementSource, double setpointSource, DoubleConsumer useOutput) {
+    super(controller, measurementSource, setpointSource, useOutput, drivetrain);
     addRequirements(drivetrain);
-    this.degrees = degrees % 360.0;
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
     drivetrain.resetGyro();
-
-    if (degrees < 180) {
-      leftVelocity *= -1;
-    } else {
-      rightVelocity *= -1;
-    }
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    // Updates target angle and resets gyro so it knows to turn back if it overshoots
-    degrees -= drivetrain.getGyroAngle();
-    drivetrain.resetGyro();
-    if (degrees < 180) {
-      leftVelocity *= -1;
-    } else {
-      rightVelocity *= -1;
-    }
-
-    drivetrain.set(ControlMode.Velocity, leftVelocity, rightVelocity);    
+    var pidOut = m_controller.calculate(m_measurement.getAsDouble(), m_setpoint.getAsDouble());
+    var ffOut = m_ff.calculate(m_measurement.getAsDouble());
+    m_useOutput.accept(pidOut + ffOut);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    super.end(interrupted);
     drivetrain.set(0, 0);
   }
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return Math.abs(drivetrain.getGyroAngle() - degrees) <= allowedError;
+  private class AlignMotorFeedforward extends SimpleMotorFeedforward {
+
+    public AlignMotorFeedforward(double ks, double kv) {
+      super(ks, kv);
+    }
+
+    public double calculate(double targetX, double notAcceleration) {
+      int outputRegion = 1;
+      if (Math.abs(drivetrain.getGyroAngle()) <= 2) {
+        targetX = 0.0;
+        outputRegion = 0;
+      }
+      var ksOut = ks * -Math.signum(targetX);
+      // if (Math.abs(targetX) <= Constants.VISION_INNER_ALIGN_THRESHOLD && 
+      //   Math.abs(targetX) <= Constants.VISION_OUTER_ALIGN_THRESHOLD) {
+      //   ksOut *= 0.5;
+      // }
+
+      SmartDashboard.putNumber("outputRegion", outputRegion);
+      
+      return ksOut + kv * targetX + ka * notAcceleration;
+    }
   }
 }
